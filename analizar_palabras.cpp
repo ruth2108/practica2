faf4038a106320bd1a9e24aac879826fb98a3b49
ruth2108 <ruth_218_@hotmail.com>
2017-10-23 10:59:15 -0500

    estructura principal

--- analizar_palabras.cpp
+++ analizar_palabras.cpp
@@ -1,1 +1,25 @@
- 
+#include <iostream>
+#include <string> 
+using namespace std;
+palindroma(string palabra)
+{
+
+
+
+}
+vcales(string palabra)
+{
+	
+	
+}
+int main(){
+	
+string palabra;
+cout<<"introduce una palabra: ";
+cin>>palabra;
+
+cout<<"La palabra : ";
+palindroma(palabra);
+cout<<"La palabra: ";
+vocales(palabra);	
+}
