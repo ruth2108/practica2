a5d855194a5b74d7394b15ed24ef42e14b705a25
Cristian Velazquez <crisg.3080@gmail.com>
2017-10-23 11:11:31 -0500

    Funcion Palindroma

--- analizar_palabras.cpp
+++ analizar_palabras.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string> 
+include "stdafx.h"
 using namespace std;
 palindroma(string palabra)
 {
+	int I = 0;
+	string  pal =palabra;
+	
+
+	int J = pal.length() - 1;
+	int mitad = J / 2;
+	while (pal[I] == pal[J]) {
+		if (mitad == I) {
+			cout << "Es palindroma";
+			break;
+		}
+		J--;
+		I++;
+	}
+	if (mitad != I)
+		cout << "No es palindroma";
 
 
 
@@ -22,4 +39,6 @@ cout<<"La palabra : ";
 palindroma(palabra);
 cout<<"La palabra: ";
 vocales(palabra);	
+system("Pause");
+return 0;
 }
